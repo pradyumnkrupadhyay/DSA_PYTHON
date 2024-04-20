82ce31810986eca202fc5ed24687f02dcee6d445
pradyumnkrupadhyay <162385556+pradyumnkrupadhyay@users.noreply.github.com>
2024-04-21 02:27:43 +0530

    Time: 35 ms (57.14%) | Memory: 16.6 MB (62.61%) - LeetSync

---
+++ 233-number-of-digit-one/number-of-digit-one.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countDigitOne(self, n: int) -> int:
+        ones, m = 0, 1
+        while m <= n:
+            ones += (n //m + 8) // 10 * m + (n // m % 10 == 1) * (n % m + 1)
+            m *= 10
+        return ones
